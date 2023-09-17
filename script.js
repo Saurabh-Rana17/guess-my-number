576a5cbae94a4262f09999ad2735fbc567f6d813
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-09-17 21:35:59 +0530

    button event added

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-'use strict';
+"use strict";
+console.clear();
+// let a = document.querySelector(".message");
+// console.log(a.textContent);
+// document.querySelector(".message").textContent = "Correct Number";
+// console.log(a.textContent);
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 12;
+// document.querySelector(".guess").value = 12;
+// console.log(document.querySelector(".guess").value);
+document.querySelector(".check").addEventListener("click", () => {
+  console.log(document.querySelector(".guess").value);
+});
